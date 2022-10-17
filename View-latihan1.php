643cd14591e198a79e3f1e3e80ac54757e3fac6e
Jeki <113359151+PrinceJeki@users.noreply.github.com>
2022-10-17 16:35:08 +0700

    Add files via upload

---
+++ View-latihan1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    halo kawan.. yuk kita belajar web programming <br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari permodelan dengan metode <? $hasil1 = "jumlah";
+    if($hasil1 == "kurang")
+    {
+        echo ("Penjumlahan");
+    }
+
+    else{
+    echo ("Pengurangan");
+    }?> yaitu <?= $nilai1. " + " . $nilai2. " = " . $hasil;?>
+</body>
+</html>
+
